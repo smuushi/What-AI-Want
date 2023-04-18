3650dba3a469052a66daaf4e0333850455d56131
smuushi <mishih780@gmail.com>
2023-04-18 13:51:20 -0400

    work

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -15,12 +15,36 @@ router.get("/all/:userId", restoreUser, async (req, res, next) => {
   const user = await User.findOne({ _id: req.params.userId });
   
   const images = await Promise.all(user.images.map(async (imageId) => {
-    const image = await Image.findOne({_id: imageId})
-    return image
+    const image = await Image.findOne({_id: imageId});
+    return image;
   }))
 
+
+  let keys = [];
+
+  images.forEach((image) => {
+    keys.push(image.AWSKey)
+  })
+
+  tempUrls = await Promise.all(
+    keys.map(async (key) => {
+      const tempUrl = await getUrlFromAwsWithKey(key);
+      return tempUrl;
+    })
+  );
+
+  const returns = images.map((imageObj, idx) => {
+    // console.log(imageObj)
+    const resObj = {...imageObj.toObject(),
+      tempUrl: tempUrls[idx]
+    };
+    
+    return resObj
+  })
+
+
   res.json({
-    images
+    returns
   })
 
 })
@@ -28,9 +52,15 @@ router.get("/all/:userId", restoreUser, async (req, res, next) => {
 router.post("/save/:imageId", restoreUser, async (req, res, next) => {
     if (!req.user) return res.json(null);
   
-    //   const user = await 
-    
+    const mongooseUser = await User.findOne({_id: req.user._id});
 
+    const image = await Image.findOne({_id: req.params.imageId});
+
+    mongooseUser.images.push(image._id);
+
+    mongooseUser.save();
+
+    return json(mongooseUser);
 })
 
 
