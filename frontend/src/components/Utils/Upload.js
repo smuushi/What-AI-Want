608d3b324f34cc6f4db5b08c10cbfa0164d94c05
useNavigate <useNavigate@gmail.com>
2023-04-19 11:13:55 -0400

    commented db change line

--- frontend/src/components/Utils/Upload.js
+++ frontend/src/components/Utils/Upload.js
@@ -1,51 +1,51 @@
-import {useState, useRef } from "react";
+// import {useState, useRef } from "react";
 
-const Upload = () => {
-    const uploadButton= useRef()
+// const Upload = () => {
+//     const uploadButton= useRef()
 
-   const [profileImage, setProfileImage] = useState(null);
+//    const [profileImage, setProfileImage] = useState(null);
 
-   const handleFile = ({ currentTarget }) => {
-     const file = currentTarget.files[0];
-     setProfileImage(file);
-   };
-
-
- const handleSubmit = async (e) => {
-   e.preventDefault();
-   const formData = new FormData();
-
-   if (profileImage) {
-     formData.append("user[profileImage]", profileImage);
-   }
-
-   const fileReader = new FileReader();
-   console.log(fileReader.readAsDataURL(profileImage));
-
-      // const response = await j(`/api/user${userId}`, {
-      //   method: "PATCH",
-      //   body: formData,
-      // });
-
-      if (response.ok) {
-        const post = await response.json();
-        setProfileImage(null);
-      }
- };
+//    const handleFile = ({ currentTarget }) => {
+//      const file = currentTarget.files[0];
+//      setProfileImage(file);
+//    };
 
 
-  return (
-    <div>
-      <input
-        ref={uploadButton}
-        type="file"
-        onChange={handleFile}
-      />
-      {console.log(profileImage)}
-      {profileImage && <img src={URL.createObjectURL(profileImage)} />}
-      <button>Upload</button>
-    </div>
-  );
-};
+//  const handleSubmit = async (e) => {
+//    e.preventDefault();
+//    const formData = new FormData();
 
-export default Upload;
+//    if (profileImage) {
+//      formData.append("user[profileImage]", profileImage);
+//    }
+
+//    const fileReader = new FileReader();
+//    console.log(fileReader.readAsDataURL(profileImage));
+
+//       // const response = await j(`/api/user${userId}`, {
+//       //   method: "PATCH",
+//       //   body: formData,
+//       // });
+
+//       if (response.ok) {
+//         const post = await response.json();
+//         setProfileImage(null);
+//       }
+//  };
+
+
+//   return (
+//     <div>
+//       <input
+//         ref={uploadButton}
+//         type="file"
+//         onChange={handleFile}
+//       />
+//       {console.log(profileImage)}
+//       {profileImage && <img src={URL.createObjectURL(profileImage)} />}
+//       <button>Upload</button>
+//     </div>
+//   );
+// };
+
+// export default Upload;
