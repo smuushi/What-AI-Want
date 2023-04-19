ee8571cbd24b3b8e2e9457b772b99c6440c1ecab
Tysuiku <timothydong41@gmail.com>
2023-04-19 10:04:16 -0400

    start profile page workflow

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,14 +1,22 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchUserLists } from "../../store/lists";
+import { getCurrentUser } from "../../store/session";
+import ProfileBox from "./ProfileBox";
 
 function Profile() {
   const dispatch = useDispatch();
   const currentUser = useSelector((state) => state.session.user);
   useEffect(() => {
     dispatch(fetchUserLists(currentUser?._id));
-  }, [currentUser]);
-  return <>profile</>;
+    dispatch(getCurrentUser());
+  }, []);
+
+  return (
+    <div>
+      <ProfileBox />
+    </div>
+  );
 }
 
 export default Profile;
