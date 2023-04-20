0cb02d775c5a96f52aa2e2b197409c01a799a176
Tysuiku <timothydong41@gmail.com>
2023-04-20 16:04:24 -0400

    logo can link back to home and scrolltotop for all links added

--- frontend/src/components/NavBar/Logo.js
+++ frontend/src/components/NavBar/Logo.js
@@ -1,13 +1,23 @@
+import React from "react";
 import "./Logo.css";
 import logoImage from "./testinglogo.png";
+import { NavLink } from "react-router-dom";
 
 const Logo = () => {
   return (
     <div className="what-ai-want-logo">
-      <p id="what-ai-logo">What</p>
-      <img src={logoImage} />
-      <p id="AI-logo">AI</p>
-      <p id="want-ai-logo">Want</p>
+      <NavLink to="/home" style={{ textDecoration: "none" }}>
+        <p id="what-ai-logo">What</p>
+      </NavLink>
+      <NavLink to="/home">
+        <img src={logoImage} alt="Logo" />
+      </NavLink>
+      <NavLink to="/home" style={{ textDecoration: "none" }}>
+        <p id="AI-logo">AI</p>
+      </NavLink>
+      <NavLink to="/home" style={{ textDecoration: "none" }}>
+        <p id="want-ai-logo">Want</p>
+      </NavLink>
     </div>
   );
 };
