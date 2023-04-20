5228dfdf2abe9c1eea5fa68f9fcef90a0f66e17a
smuushi <mishih780@gmail.com>
2023-04-20 11:32:26 -0400

    housing

--- frontend/src/store/lists.js
+++ frontend/src/store/lists.js
@@ -40,7 +40,7 @@ export const fetchUserLists = (userId) => async (dispatch) => {
   const response = await jwtFetch(`/api/lists/all/${userId}`);
   if (response.ok) {
     const lists = await response.json();
-    debugger
+    // debugger
     dispatch(receiveLists(lists));
   }
 };
