bcf827e376f15d690efc1b966fd0af7ca0aaf5e1
Tysuiku <timothydong41@gmail.com>
2023-04-20 13:37:33 -0400

    start logo and changed navbar size

---
+++ frontend/src/components/NavBar/Logo.js
@@ -0,0 +1,8 @@
+const Logo = () => {
+  <div className="what-ai-want-logo">
+    <p id="what-ai-logo">What</p>
+    <img src={logoImage} />
+    <p id="AI-logo">AI</p>
+    <p id="want-ai-logo">Want</p>
+  </div>;
+};
