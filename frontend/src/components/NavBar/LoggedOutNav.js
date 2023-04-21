03c697ad2bad7234936c96a2030e30f0d4eef48e
useNavigate <useNavigate@gmail.com>
2023-04-20 21:46:53 -0400

    styling MAike

--- frontend/src/components/NavBar/LoggedOutNav.js
+++ frontend/src/components/NavBar/LoggedOutNav.js
@@ -11,13 +11,13 @@ const LoggedOutNav = () => {
         <div className="logged-out-logo">
           <Logo />
         </div>
-        <div id="signup">
-          <p>Make your Aivatar Today!</p>
+        {/* <div id="signup">
+          <p>Make your Aivatar Today!</p> */}
           <div id="userAuthButtons">
             <SignInUpModal type={signUp} />
             <SignInUpModal id="login" type={login} />
           </div>
-        </div>
+        {/* </div> */}
       </div>
     </>
   );
